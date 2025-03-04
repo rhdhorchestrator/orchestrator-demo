9c027780347ca182d4e578ba90465a1801d48135
Jonathan Kilzi <jkilzi@redhat.com>
2025-03-04 22:59:23 +0200

    Refactor based on feedback from the PR and manual testing
    Signed-off-by: Jonathan Kilzi <jkilzi@redhat.com>

--- scripts/lib/_functions.sh
+++ scripts/lib/_functions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function is_macos {
-    [[ "$(uname)" == "Darwin" ]]
+    [[ "$(uname)" == Darwin ]]
 }
 
 # A wrapper for the find command that uses the -E flag on macOS.
@@ -15,19 +15,19 @@ function findw {
 }
 
 function get_workflow_id {
-    local workdir=${1:-$PWD}
+    local workdir="$1"
     local workflow_file=""
     local workflow_id=""
 
     workflow_file=$(findw "$workdir" -type f -regex '.*\.sw\.ya?ml$')
     if [ -z "$workflow_file" ]; then
-        echo "ERROR: No workflow file found with *.sw.yaml or *.sw.yml suffix"
+        log_error "No workflow file found with *.sw.yaml or *.sw.yml suffix"
         return 10
     fi
 
     workflow_id=$(yq '.id | downcase' "$workflow_file" 2>/dev/null)
     if [ -z "$workflow_id" ]; then
-        echo "ERROR: The workflow file doesn't seem to have an 'id' property."
+        log_error "The workflow file doesn't seem to have an 'id' property."
         return 11
     fi
 
@@ -36,4 +36,13 @@ function get_workflow_id {
 
 function pocker {
     $(command -v podman || command -v docker) "$@"
+}
+
+function assert_optarg_not_empty {
+    local arg="$1"
+
+    if [[ -z "${arg#*=}" ]]; then
+        log_error "Option --${arg%=*} requires an argument when specified."
+        return 12
+    fi
 }
